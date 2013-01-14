d8a87e786aaf0e483b28e66eeb67aafdd593695f
meister <meister.technoid@gmail.com>
2013-01-14 17:09:47 +0400

    no message

--- views/user/login.php
+++ views/user/login.php
@@ -19,33 +19,32 @@ $this->breadcrumbs=array(
 
 <div class="form">
 <?php echo CHtml::beginForm(); ?>
-
 	<p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
 	
 	<?php echo CHtml::errorSummary($model); ?>
 	
-	<div class="row">
+	<div class="">
 		<?php echo CHtml::activeLabelEx($model,'username'); ?>
 		<?php echo CHtml::activeTextField($model,'username') ?>
 	</div>
 	
-	<div class="row">
+	<div class="">
 		<?php echo CHtml::activeLabelEx($model,'password'); ?>
 		<?php echo CHtml::activePasswordField($model,'password') ?>
 	</div>
 	
-	<div class="row">
+	<div class="">
 		<p class="hint">
 		<?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?> | <?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?>
 		</p>
 	</div>
 	
-	<div class="row rememberMe">
+	<div class=" rememberMe">
 		<?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
 		<?php echo CHtml::activeLabelEx($model,'rememberMe'); ?>
 	</div>
 
-	<div class="row submit">
+	<div class=" submit">
 		<?php echo CHtml::submitButton(UserModule::t("Login")); ?>
 	</div>
 	
